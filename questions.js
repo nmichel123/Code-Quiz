0dbdf9c34bef3019e6c8cf67fe6cacce81287e8d
Nico Michel <nicomichel@utexas.edu>
2020-06-23 23:45:49 -0500

    uploading readme as well as image to submit for grading

---
+++ questions.js
@@ -0,0 +1,23 @@
+var questions = [
+    {
+        title: "Which animal do you prefer?",
+        choices: ["cat", "dog", "gator", "croc"],
+        answer: "gator",
+    },
+    {
+        title: "What shoes do you wear?",
+        choices: ["crocs", "toms", "converse", "barefoot advocate"],
+        answer: "toms",
+    },
+    {
+        title: "Favorite Coffee?",
+        choices: ["espresso", "chai latte", "black", "with cream"],
+        answer: "espresso",
+    },
+    {
+        title: "Favorite pastime?",
+        choices: ["play guitar", "play videogames", "walk", "watch tv"],
+        answer: "walk", 
+    }
+
+]
