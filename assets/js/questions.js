92dcb296f88314e59816d5be6aea31317371f8ca
Nico Michel <nicomichel@utexas.edu>
2020-10-30 18:26:50 -0500

    updated questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,23 +1,43 @@
 var questions = [
     {
-        title: "Which animal do you prefer?",
-        choices: ["cat", "dog", "gator", "croc"],
-        answer: "gator",
+        title: "How many individuals die at the hand of Jason in the first Friday the 13th movie?",
+        choices: ["Zero", "One", "Two", "Three"],
+        answer: "Zero",
     },
     {
-        title: "What shoes do you wear?",
-        choices: ["crocs", "toms", "converse", "barefoot advocate"],
-        answer: "toms",
+        title: "What is the room number for the room that Dick Hallorann tells Danny to stay away from in The Shining?",
+        choices: ["562", "237", "104", "319"],
+        answer: "237",
     },
     {
-        title: "Favorite Coffee?",
-        choices: ["espresso", "chai latte", "black", "with cream"],
-        answer: "espresso",
+        title: "How many Michael Myers Halloween movies are there?",
+        choices: ["Eight", "Three", "Seven", "Nine"],
+        answer: "Nine",
     },
     {
-        title: "Favorite pastime?",
-        choices: ["play guitar", "play videogames", "walk", "watch tv"],
-        answer: "walk", 
+        title: "What color is Freddy Kreuger’s sweater?",
+        choices: ["Black", "Red and Green", "Red", "Black and Red"],
+        answer: "Red and Green", 
+    },
+    {
+        title: "What hobby does Norman enjoy in Alfred Hitchcock's Psycho?",
+        choices: ["Golf", "Upholstery", "Taxidermy", "Stocks"],
+        answer: "Taxidermy",
+    },
+    {
+        title: "How many people died during the production of The Exorcist?",
+        choices: ["Forty", "Three", "Nine", "Seven"],
+        answer: "Nine",
+    },
+    {
+        title: "Which of these films were NOT made by David Cronenberg?",
+        choices: ["The Fly", "eXistenZ", "A History of Violence", "Halloween"],
+        answer: "Halloween",
+    },
+    {
+        title: "Which actor played the leading man in both Se7en and Interview with a Vampire?",
+        choices: ["Tom Cruise", "Morgan Freeman", "Clint Eastwood", "Brad Pitt"],
+        answer: "Brad Pitt", 
     }
 
 ];
